b9106822e65ee301aaf12f0c47e1b595d3b64b9a
User <user@example.com>
2026-09-08 08:40:20 +0530

    feat: support Vercel Postgres and cloud databases

--- blog/settings.py
+++ blog/settings.py
@@ -91,7 +91,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        env='POSTGRES_URL',
+        default=os.environ.get('DATABASE_URL', 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
         conn_max_age=600,
         conn_health_checks=True,
     )
